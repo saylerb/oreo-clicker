5d78c45bff10edfdc554e8cb8e8a24a6b77eea9a
Brian Sayler <sayler.b@gmail.com>
2018-10-06 14:32:01 -0500

    add depress affect on active

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { render } from "react-dom";
 import SVGInline from "react-svg-inline";
 import oreo from "./oreo.svg";
 
+import "./styles.css";
+
 const App = () => (
-  <div>
-    <SVGInline width={400} svg={oreo} />
+  <div className="App">
+    <SVGInline className="oreo" classSuffix="-svg" width={400} svg={oreo} />
   </div>
 );
 
