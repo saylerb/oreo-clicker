e2cece1491cc3b74ebbd13ed816e54846bcfa59d
Brian Sayler <sayler.b@gmail.com>
2018-10-06 14:16:28 -0500

    add oreo svg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import SVGInline from "react-svg-inline";
+import oreo from "./oreo.svg";
 
-const App = () => <div>Hello, World</div>;
+const App = () => (
+  <div>
+    <SVGInline width={400} svg={oreo} />
+  </div>
+);
 
 render(<App />, document.getElementById("root"));
