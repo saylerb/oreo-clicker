99a9b6cbfb280c3fdd84d29812bdbfe8b15d988c
Brian Sayler <sayler.b@gmail.com>
2018-10-06 13:44:23 -0500

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 
-const App = () => (
-    <div>Hello, World</div>
-)
+const App = () => <div>Hello, World</div>;
 
-render(<App/>, document.getElementById("root"))
+render(<App />, document.getElementById("root"));
