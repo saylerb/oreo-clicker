f740c6d731e980040ba7510047710ac77dad075d
Brian Sayler <sayler.b@gmail.com>
2018-10-07 14:02:26 -0500

    make counter stateful

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Counter = () => <div data-testid="current-count">Count: 0</div>;
+class Counter extends React.Component {
+  render() {
+    return <div data-testid="current-count">Count: 0</div>;
+  }
+}
 
 export default Counter;
